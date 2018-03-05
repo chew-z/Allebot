e29137b0ab600585b5574b677ec6a330b89e2ea1
Robert J <chew-z@users.noreply.github.com>
2018-03-05 21:17:05 +0100

    Proof of Concept v 0.0.1

--- assistant-webhook.py
+++ assistant-webhook.py
@@ -1,36 +1,145 @@
-#!/usr/bin/env python3
-
-
+# -*- coding: utf-8 -*-
 import logging
 from flask import Flask
-from flask_assistant import Assistant, ask, tell
+from zeep import Client
+import configparser
+import os
+config = configparser.ConfigParser()
+config.read('allegro.conf')
+google_conf = config['Google']
+os.environ['DEV_ACCESS_TOKEN'] = google_conf['DEV_ACCESS_TOKEN']
+os.environ['CLIENT_ACCESS_TOKEN'] = google_conf['CLIENT_ACCESS_TOKEN']
+
+from flask_assistant import Assistant, ask, tell, build_item
+
 
 app = Flask(__name__)
-assist = Assistant(app, route='/colors')
+app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
+assist = Assistant(app, route='/')
 logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 
 
-@assist.action('greeting')
-def greet_and_start():
-    speech = "Hey! Are you male or female?"
-    return ask(speech)
+allegro_conf = config['Allegro']
+
+countryId = 1
+webAPI = allegro_conf['WEB_API_KEY']
+url = allegro_conf['WEB_API_WSDL']
+client = Client(url)
 
 
-@assist.action("give-gender")
-def ask_for_color(gender):
-    if gender == 'male':
-        gender_msg = 'Sup bro!'
-    else:
-        gender_msg = 'Haay gurl!'
+def alleSearch(q, category=2, condition='used', size=5):
 
-    speech = gender_msg + ' What is your favorite color?'
-    return ask(speech)
+    filterOptionsList = {'item':
+            {'filterId': 'category',
+                'filterValueId': [category]
+                },
+            'item':
+            {'filterId': 'condition',
+                'filterValueId': condition
+                },
+            'item':
+            {'filterId': 'search',
+                'filterValueId': q
+                }
+            }
+
+    wynik = client.service.doGetItemsList(
+            webAPI, countryId, filterOptions=filterOptionsList, resultScope=3, resultSize=size)
+    logging.info("Otrzymano %d wynikow." % wynik.itemsCount)
+    l = list()
+    if wynik.itemsList is not None:
+        for item in wynik.itemsList['item']:
+            logging.info("{} --- {} --- {}".format(item.itemTitle, item.timeToEnd, item.priceInfo['item'][0]['priceValue']))
+            logging.info('https://allegro.pl/i' + str(item.itemId) + '.html')
+            typAukcji = ''
+            if item.priceInfo['item'][0]['priceType'] == 'buyNow':
+                typAukcji = 'kup teraz'
+            else:
+                typAukcji = 'aukcja'
+            d = {
+                    'aukcja': item.itemTitle,
+                    'cena': item.priceInfo['item'][0]['priceValue'],
+                    'czas do końca': item.timeToEnd,
+                    'id': item.itemId,
+                    'link': 'https://allegro.pl/i' + str(item.itemId) + '.html',
+                    'typ aukcji': typAukcji,
+                    'zdjęcie': item.photosInfo['item'][0]['photoUrl']
+                    }
+            l.append(d)
+    print(l)
+    return(l)
 
 
-@assist.action('give-color', mapping={'color': 'sys.color'})
-def ask_for_season(color):
-    speech = 'Ok, {} is an okay color I guess'.format(color)
-    return ask(speech)
+@assist.action('Default Welcome Intent')
+def welcome():
+    speech = 'Welcome to Allegro shopping on Google Assistant!'
+    return ask(speech).reprompt('Do you want to see examples?').suggest('Search iphone', 'Search macbook')
+
+
+@assist.action('Default Welcome Intent - yes')
+def action_func():
+    speech = """What would you like to search for?.
+                    Make your choice!"""
+
+    return ask(speech).suggest('iPhone', 'MacBook')
+
+
+@assist.action('SearchIphone')
+# show results in Carousel
+def action_func():
+    resp = ask('Here are some iPhones I have found on Allegro').build_carousel()
+    searchResult = alleSearch('iPhone', size=3)
+    i = 0
+    for item in searchResult:
+        i += 1
+        resp.add_item("{} zł".format(item['cena']), 
+                key='option {}'.format(i),
+                img_url=item['zdjęcie'],
+                description=item['aukcja'])
+    
+    return resp
+
+
+
+@assist.action('SearchMacbook')
+def action_func():
+
+    # Basic speech/text response
+    resp = ask("Let me show you some Macbooks available")
+    searchResult = alleSearch('Macbook', size=3)
+
+    # Create a list with a title
+    mylist = resp.build_list('Macbook')
+    i = 0
+    for item in searchResult:
+        i += 1
+        mylist.add_item("{} zł".format(item['cena']), 
+                key='option {}'.format(i),
+                img_url=item['zdjęcie'],
+                description=item['aukcja'],
+                synonyms=[])
+
+    return mylist
+
+@assist.action('SearchAnything', mapping={'search_item': 'sys.any'})
+def action_func(search_item):
+
+    # Basic speech/text response
+    resp = ask("Let's see what is available")
+    searchResult = alleSearch("{}".format(search_item), size=5)
+
+    # Create a list with a title
+    mylist = resp.build_list('Results')
+    i = 0
+    for item in searchResult:
+        i += 1
+        mylist.add_item("{} zł".format(item['cena']), 
+                key='option {}'.format(i),
+                img_url=item['zdjęcie'],
+                description=item['aukcja'],
+                synonyms=[])
+
+    return mylist
 
 
 if __name__ == '__main__':
