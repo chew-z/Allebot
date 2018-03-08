5eef265c62a55997d64dc740e2bc64515a9c913f
Robert J <chew-z@users.noreply.github.com>
2018-03-08 10:30:41 +0100

    re-formatted

--- assistant-webhook.py
+++ assistant-webhook.py
@@ -30,26 +30,27 @@ client = Client(url)
 def alleSearch(q, category=2, condition='used', size=5):
 
     filterOptionsList = {'item':
-            {'filterId': 'category',
-                'filterValueId': [category]
-                },
-            'item':
-            {'filterId': 'condition',
-                'filterValueId': condition
-                },
-            'item':
-            {'filterId': 'search',
-                'filterValueId': q
-                }
-            }
+                         {'filterId': 'category',
+                          'filterValueId': [category]
+                          },
+                         'item':
+                         {'filterId': 'condition',
+                          'filterValueId': condition
+                          },
+                         'item':
+                         {'filterId': 'search',
+                          'filterValueId': q
+                          }
+                         }
 
     wynik = client.service.doGetItemsList(
-            webAPI, countryId, filterOptions=filterOptionsList, resultScope=3, resultSize=size)
+        webAPI, countryId, filterOptions=filterOptionsList, resultScope=3, resultSize=size)
     logging.info("Otrzymano %d wynikow." % wynik.itemsCount)
     l = list()
     if wynik.itemsList is not None:
         for item in wynik.itemsList['item']:
-            logging.info("{} --- {} --- {}".format(item.itemTitle, item.timeToEnd, item.priceInfo['item'][0]['priceValue']))
+            logging.info("{} --- {} --- {}".format(item.itemTitle,
+                                                   item.timeToEnd, item.priceInfo['item'][0]['priceValue']))
             logging.info('https://allegro.pl/i' + str(item.itemId) + '.html')
             typAukcji = ''
             if item.priceInfo['item'][0]['priceType'] == 'buyNow':
@@ -57,14 +58,14 @@ def alleSearch(q, category=2, condition='used', size=5):
             else:
                 typAukcji = 'aukcja'
             d = {
-                    'aukcja': item.itemTitle,
-                    'cena': item.priceInfo['item'][0]['priceValue'],
-                    'czas do końca': item.timeToEnd,
-                    'id': item.itemId,
-                    'link': 'https://allegro.pl/i' + str(item.itemId) + '.html',
-                    'typ aukcji': typAukcji,
-                    'zdjęcie': item.photosInfo['item'][0]['photoUrl']
-                    }
+                'aukcja': item.itemTitle,
+                'cena': item.priceInfo['item'][0]['priceValue'],
+                'czas do końca': item.timeToEnd,
+                'id': item.itemId,
+                'link': 'https://allegro.pl/i' + str(item.itemId) + '.html',
+                'typ aukcji': typAukcji,
+                'zdjęcie': item.photosInfo['item'][0]['photoUrl']
+            }
             l.append(d)
     print(l)
     return(l)
@@ -73,7 +74,8 @@ def alleSearch(q, category=2, condition='used', size=5):
 @assist.action('Default Welcome Intent')
 def welcome():
     speech = 'Welcome to Allegro shopping on Google Assistant!'
-    return ask(speech).reprompt('Do you want to see examples?').suggest('Search iphone', 'Search macbook')
+    return ask(speech).reprompt('Do you want to see examples?').suggest(
+        'Search iphone', 'Search macbook')
 
 
 @assist.action('Default Welcome Intent - yes')
@@ -92,13 +94,12 @@ def action_func():
     i = 0
     for item in searchResult:
         i += 1
-        resp.add_item("{} zł".format(item['cena']), 
-                key='option {}'.format(i),
-                img_url=item['zdjęcie'],
-                description=item['aukcja'])
-    
-    return resp
+        resp.add_item("{} zł".format(item['cena']),
+                      key='option {}'.format(i),
+                      img_url=item['zdjęcie'],
+                      description=item['aukcja'])
 
+    return resp
 
 
 @assist.action('SearchMacbook')
@@ -113,13 +114,14 @@ def action_func():
     i = 0
     for item in searchResult:
         i += 1
-        mylist.add_item("{} zł".format(item['cena']), 
-                key='option {}'.format(i),
-                img_url=item['zdjęcie'],
-                description=item['aukcja'],
-                synonyms=[])
+        mylist.add_item("{} zł".format(item['cena']),
+                        key='option {}'.format(i),
+                        img_url=item['zdjęcie'],
+                        description=item['aukcja'],
+                        synonyms=[])
 
     return mylist
+
 
 @assist.action('SearchAnything', mapping={'search_item': 'sys.any'})
 def action_func(search_item):
@@ -133,11 +135,11 @@ def action_func(search_item):
     i = 0
     for item in searchResult:
         i += 1
-        mylist.add_item("{} zł".format(item['cena']), 
-                key='option {}'.format(i),
-                img_url=item['zdjęcie'],
-                description=item['aukcja'],
-                synonyms=[])
+        mylist.add_item("{} zł".format(item['cena']),
+                        key='option {}'.format(i),
+                        img_url=item['zdjęcie'],
+                        description=item['aukcja'],
+                        synonyms=[])
 
     return mylist
 
