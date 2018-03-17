33d77470857085d40ed50cfb358eed7d2a2cb833
Robert J <chew-z@users.noreply.github.com>
2018-03-17 13:10:05 +0100

    Moved dialog-webhook to appengine

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
-Test of Google AppEngine
-simple webhook for searching Allegro
-- via Allegro Web API
-- uses zeep
-- returns item url
-'''
+# import logging.config
+import logging
+import configparser
+import json
+import os
+from pathlib import Path
 from flask import Flask, request
 from flask.json import jsonify
 from zeep import Client
 from zeep.cache import SqliteCache
 from zeep.transports import Transport
-import json
-from pathlib import Path
-import argparse
-import configparser
-import logging
-import logging.config
-import os
-
 config = configparser.ConfigParser()
 config.read('allebot.conf')
-allegro_conf = config['Allegro']
-
-countryId = 1
-webAPI = allegro_conf['WEB_API_KEY']
-url = allegro_conf['WEB_API_WSDL']
-# Can't use sql cache on AppEngine
-# https://github.com/mvantellingen/python-zeep/issues/243
-client = Client(url, transport=Transport(cache=None))
-
+google_conf = config['Google']
+os.environ['DEV_ACCESS_TOKEN'] = google_conf['DEV_ACCESS_TOKEN']
+os.environ['CLIENT_ACCESS_TOKEN'] = google_conf['CLIENT_ACCESS_TOKEN']
+# Dialogflow agent tokens need to be set at flask_assistant import 
+from flask_assistant import Assistant, ask, tell, build_item
+from flask_assistant import context_manager
 
 app = Flask(__name__)
 # https://stackoverflow.com/questions/36378441/
 # preserve Unicode in jsonify
 app.config['JSON_AS_ASCII'] = False
+app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
+assist = Assistant(app, route='/')
+
+allegro_conf = config['Allegro']
+countryId = 1
+webAPI = allegro_conf['WEB_API_KEY']
+url = allegro_conf['WEB_API_WSDL']
+
+# cache = SqliteCache(path='/tmp/dialog-webhook_zeep_sqlite.db', timeout=60)
+# transport = Transport(cache=cache)
+# client = Client(url, transport=transport)
+# Can't use sql cache on AppEngine
+# https://github.com/mvantellingen/python-zeep/issues/243
+client = Client(url, transport=Transport(cache=None))
 
 
 def alleKategorieGet():
@@ -58,6 +59,7 @@ def alleKategorieGet():
 
   return(j)
 
+
 def alleKategoriaNazwa(categories, id):
   ''' Returns category name from id
   '''
@@ -78,7 +80,7 @@ def alleKategorieBranch(categories, id):
     return ''
   else:
     # continue recursive pattern following the branch
-    # logging.debug("itemId {} - up - {}".format(id, up[0]))
+    logging.debug("itemId {} - up - {}".format(id, up[0]))
     return "{}({}) - {}".format(up[0]['catName'], up[0]['catId'], alleKategorieBranch(categories, up[0]['catParent']))
 
 
@@ -162,37 +164,111 @@ def alleSearch(q, category, size=5):
   return(_results)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@assist.action('Default Welcome Intent')
+def welcome():
+    speech = 'Welcome to Allegro shopping!'
+    return ask(speech).reprompt('Do you want to see examples?').suggest(
+        'I want new smartphone', 'I am looking for a laptop', 'Looking for bike')
+
+
+@assist.action('Welcome')
+def welcome():
+    speech = 'Welcome to Allegro shopping! You want to search for smartphone, laptop, or bike?'
+    return ask(speech)
+
+
+@assist.action('Declare-category')
+def search_category(search_category):
+    speech = "Ok, you chose {} right?".format(search_category)
+    context_manager.add('search_context', lifespan=5)
+    context_manager.set('search_context', 'search_category', search_category) 
+    return ask(speech)
+
+
+@assist.action('Confirm-category')
+def confirm_category(category_confirmation):
+    category = context_manager.get_param('search_context', 'search_category')
+    logging.info("Confirm-category: category: {}".format(category))
+    if 'n' in category_confirmation:
+        return ask('I dont think I understood. What do you want to look for?').suggest(
+        'smartphone', 'laptop', 'bike')
+    else:
+        return ask('OK. What exactly are you looking for?')
+
+
+@assist.action('SearchAnything', mapping={'search_item': 'sys.any'})
+def action_func(search_item):
+    category = context_manager.get_param('search_context', 'search_category')
+    logging.info('search_category: --{}--'.format(category))
+    sc = 0
+    if category == 'bike':
+        sc = 3919
+    elif category == 'laptop':
+        sc = 491
+    elif category == 'smartphone':
+        sc = 165
+    else:
+        sc = 0
+    # Basic speech/text response
+    resp = ask("Let's see what is available")
+    searchResult = alleSearch("{}".format(search_item), category=sc, size=10)
+
+    # Create a list with a title
+    mylist = resp.build_list('Results')
+    i = 0
+    for item in searchResult:
+        i += 1
+        mylist.add_item("{} zł".format(item['cena']),
+                        key='option {}'.format(i),
+                        img_url=item['zdjęcie'],
+                        description=item['aukcja'],
+                        synonyms=[])
+
+    return mylist
+
+
+@app.route('/hello')
+def test_page():
+    logging.info('/hello')
+    return "Hello World from dialog-webhook.py"
 
 
 @app.route('/search')
 def search():
+    logging.info('/search')
     query = request.args.get('q', default = 'iphone', type = str)
-    category = request.args.get('cat', default = 2, type = int)
+    category = request.args.get('cat', default = 48978, type = int)
     # condition = request.args.get('condition', default = 'used', type = str)
-    size = request.args.get('size', default = 5, type = int)
+    size = request.args.get('size', default = 10, type = int)
     logging.info("{} {} {}".format(query, category, size))
     result = alleSearch(query, category, size)
     
     return(jsonify(result))
 
 
-if __name__ == "__main__":
-    FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
-    try:
-        os.remove('allequery.log')
-    except BaseException:
-        pass
-    logging.basicConfig(filename='allequery.log', level=logging.DEBUG,
-            format=FORMAT, datefmt='%a, %d %b %Y %H:%M:%S', )
+if __name__ == '__main__':
+    production_environment =os.getenv('SERVER_SOFTWARE')
+    if production_environment is not None and production_enviroment.startswith('Google App Engine/'):
+        import google.cloud.logging # Doesn't conflict with standard logging
+        # Instantiates a google.logging client
+        client = google.cloud.logging.Client()
+        # google.cloud.logging.Client(app.config['PROJECT_ID'])
+        # Connects the logger to the root logging handler; by default this captures
+        # all logs at INFO level and higher
+        client.setup_logging()
+    else:
+	# flask app.debug=True in development only (not a good idea on GAE)
+        app.debug = True
+        try:
+            os.remove('allebot.log')
+        except BaseException:
+            pass
+        FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+        logging.basicConfig(filename='allebot.log', level=logging.DEBUG,
+            format=FORMAT, datefmt='%a, %d %b %Y %H:%M:%S', )    
     # Turn off default zeep.transports DEBUG logging
     logging.getLogger('zeep.transports').setLevel(logging.INFO)
+    logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
     logging.info('--- logging started ---.')
-
-    app.run(debug=True, port=8080)
-
+    app.run(port=8080)
     logging.info('--- logging finished ---.')
-
-
