234ed66a789e5bac7bb209f7eb26ba56e32f9596
Robert J <chew-z@users.noreply.github.com>
2018-03-18 20:37:58 +0100

    Figured out logging with flask, gunicorn in GAE

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-# import logging.config
+
 import logging
+import logging.config
 import configparser
 import json
+import sys
 import os
 from pathlib import Path
 from flask import Flask, request
 from flask.json import jsonify
+from flask_assistant import logger
 from zeep import Client
 from zeep.cache import SqliteCache
 from zeep.transports import Transport
@@ -24,6 +27,8 @@ app = Flask(__name__)
 # preserve Unicode in jsonify
 app.config['JSON_AS_ASCII'] = False
 app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
+
+
 assist = Assistant(app, route='/')
 
 allegro_conf = config['Allegro']
@@ -39,23 +44,67 @@ url = allegro_conf['WEB_API_WSDL']
 client = Client(url, transport=Transport(cache=None))
 
 
+def use_logging_handler():
+    # Imports the Google Cloud client library
+    import google.cloud.logging
+    # Instantiates a client
+    client = google.cloud.logging.Client()
+    # Connects the logger to the root logging handler; 
+    # by default this captures
+    # all logs at INFO level and higher
+    client.setup_logging()
+
+def isAppEngine():
+    server = os.getenv('SERVER_SOFTWARE', '')
+    if server is not None and server.startswith('Google App Engine/'): 
+        return True 
+    else: 
+        return False
+
+
+if isAppEngine():
+    use_logging_handler()
+
+ISAPPENGINE = isAppEngine()
+
+def _info(msg):
+    if ISAPPENGINE:
+        logger.info(msg)
+    else:
+        logging.info(msg)
+
+
+def _debug(msg):
+    if ISAPPENGINE:
+        logger.debug(msg)
+    else:
+        logging.debug(msg)
+
+
+def _error(msg):
+    if ISAPPENGINE:
+        logger.info(msg)
+    else:
+        logging.info(msg)
+
+
 def alleKategorieGet():
   ''' Get list of all categories from file if exists
       and if not call WebAPI
   '''
   if Path("kategorie.json").is_file():
     # file exists
-    logging.info('kategorie.json exists')
+    _debug('kategorie.json exists')
     j = json.load(open('kategorie.json'))
   else:
     # call WebAPI
-    logging.info('Couldn\'t find kategorie.json - getting from WebAPI')
+    _debug('Couldn\'t find kategorie.json - getting from WebAPI')
     wynik = client.service.doGetCatsData(webapiKey=webAPI, countryId=countryId, onlyLeaf=False)
     categories = wynik['catsList']['item']
     j = [{"catId": i['catId'], "catName": i['catName'], "catParent": i['catParent'], "catPosition": i['catPosition']} for i in categories]
     with open('kategorie.json', 'w') as outfile:
       json.dump(j, outfile)
-    logging.info('Categories saved to kategorie.json')
+    _debug('Categories saved to kategorie.json')
 
   return(j)
 
@@ -80,7 +129,7 @@ def alleKategorieBranch(categories, id):
     return ''
   else:
     # continue recursive pattern following the branch
-    logging.debug("itemId {} - up - {}".format(id, up[0]))
+    _debug("itemId {} - up - {}".format(id, up[0]))
     return "{}({}) - {}".format(up[0]['catName'], up[0]['catId'], alleKategorieBranch(categories, up[0]['catParent']))
 
 
@@ -102,7 +151,7 @@ def alleCreateFilters(filtersList):
   filterArrayPlaceholder = client.get_type('ns0:ArrayOfFilteroptionstype')
   filters = filterArrayPlaceholder()
   for key, value in filtersList.items():
-      logging.info("filters: {} = {}".format(key, value))
+      _info("filters: {} = {}".format(key, value))
       optionsArrayPlaceholder = client.get_type('ns0:FilterOptionsType')
       option = optionsArrayPlaceholder()
       AOSPlaceholder = client.get_type('ns0:ArrayOfString')
@@ -114,7 +163,7 @@ def alleCreateFilters(filtersList):
       option['filterValueId'] = AOS
       filters['item'].append(option)           
       
-  logging.info("ArrayOfFilteroptionstype object: {}".format(filters))
+  _debug("ArrayOfFilteroptionstype object: {}".format(filters))
   
   return filters 
 
@@ -129,20 +178,20 @@ def alleSearch(q, category, size=5):
   wynik = client.service.doGetItemsList(
       webAPI, countryId, filterOptions=filters, resultScope=3, resultSize=size)
   
-  logging.info("Otrzymano %d wynikow." % wynik.itemsCount)
+  _debug("Otrzymano %d wynikow." % wynik.itemsCount)
   # TODO - figure out how to display filters....
-  logging.info("filtersList: {}".format(wynik.filtersList))
-  logging.info("filtersRejected: {}".format(wynik.filtersRejected))
-  logging.info("categoriesList: {}".format(wynik.categoriesList))
+  _debug("filtersList: {}".format(wynik.filtersList))
+  _debug("filtersRejected: {}".format(wynik.filtersRejected))
+  _debug("categoriesList: {}".format(wynik.categoriesList))
 
   _results = list()
   if wynik.itemsList is not None:
       # get ready list of categories
       listOfCategories = alleKategorieGet()
       for item in wynik.itemsList['item']:
-          logging.info("{} --- {} --- {}".format(item.itemTitle,
+          _debug("{} --- {} --- {}".format(item.itemTitle,
                                                  item.timeToEnd, item.priceInfo['item'][0]['priceValue']))
-          logging.info('https://allegro.pl/i' + str(item.itemId) + '.html')
+          _debug('https://allegro.pl/i' + str(item.itemId) + '.html')
           typAukcji = ''
           if item.priceInfo['item'][0]['priceType'] == 'buyNow':
               typAukcji = 'kup teraz'
@@ -160,7 +209,7 @@ def alleSearch(q, category, size=5):
               'zdjęcie': item.photosInfo['item'][0]['photoUrl']
           }
           _results.append(d)
-  logging.info("{}".format(_results))
+  _debug("{}".format(_results))
   return(_results)
 
 
@@ -188,7 +237,7 @@ def search_category(search_category):
 @assist.action('Confirm-category')
 def confirm_category(category_confirmation):
     category = context_manager.get_param('search_context', 'search_category')
-    logging.info("Confirm-category: category: {}".format(category))
+    _debug("Confirm-category: category: {}".format(category))
     if 'n' in category_confirmation:
         return ask('I dont think I understood. What do you want to look for?').suggest(
         'smartphone', 'laptop', 'bike')
@@ -199,7 +248,7 @@ def confirm_category(category_confirmation):
 @assist.action('SearchAnything', mapping={'search_item': 'sys.any'})
 def action_func(search_item):
     category = context_manager.get_param('search_context', 'search_category')
-    logging.info('search_category: --{}--'.format(category))
+    _debug('search_category: --{}--'.format(category))
     sc = 0
     if category == 'bike':
         sc = 3919
@@ -229,46 +278,47 @@ def action_func(search_item):
 
 @app.route('/hello')
 def test_page():
-    logging.info('/hello')
+    _info('/hello')
     return "Hello World from dialog-webhook.py"
 
 
 @app.route('/search')
 def search():
-    logging.info('/search')
+    _info('/search')
     query = request.args.get('q', default = 'iphone', type = str)
     category = request.args.get('cat', default = 48978, type = int)
     # condition = request.args.get('condition', default = 'used', type = str)
     size = request.args.get('size', default = 10, type = int)
-    logging.info("{} {} {}".format(query, category, size))
+    _debug("{} {} {}".format(query, category, size))
     result = alleSearch(query, category, size)
     
     return(jsonify(result))
 
 
+@app.errorhandler(500)
+def server_error(e):
+    logger.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
 if __name__ == '__main__':
-    production_environment =os.getenv('SERVER_SOFTWARE')
-    if production_environment is not None and production_enviroment.startswith('Google App Engine/'):
-        import google.cloud.logging # Doesn't conflict with standard logging
-        # Instantiates a google.logging client
-        client = google.cloud.logging.Client()
-        # google.cloud.logging.Client(app.config['PROJECT_ID'])
-        # Connects the logger to the root logging handler; by default this captures
-        # all logs at INFO level and higher
-        client.setup_logging()
-    else:
-	# flask app.debug=True in development only (not a good idea on GAE)
-        app.debug = True
-        try:
-            os.remove('allebot.log')
-        except BaseException:
-            pass
-        FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
-        logging.basicConfig(filename='allebot.log', level=logging.DEBUG,
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    # flask app.debug=True in development only (not a good idea on GAE)
+    app.debug = True
+    try:
+        os.remove('allebot.log')
+    except BaseException:
+        pass
+    FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(filename='allebot.log', level=logging.DEBUG,
             format=FORMAT, datefmt='%a, %d %b %Y %H:%M:%S', )    
     # Turn off default zeep.transports DEBUG logging
     logging.getLogger('zeep.transports').setLevel(logging.INFO)
     logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
-    logging.info('--- logging started ---.')
+    _info('--- logging started ---.')
     app.run(port=8080)
-    logging.info('--- logging finished ---.')
+    _info('--- logging finished ---.')
